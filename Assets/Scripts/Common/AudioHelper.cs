67d1c9fa8e72eebc1c8b8cc3e9ca37e50b269f0b
Henry Hazlett <ticotoad@gmail.com>
2020-08-24 15:27:10 -0500

    Set up player Health and Enemy

---
+++ Assets/Scripts/Common/AudioHelper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class AudioHelper
+{
+    public static AudioSource PlayClip2D(AudioClip clip, float volume)
+    {
+        GameObject audioObject = new GameObject("Audio2D");
+        AudioSource audioSource = audioObject.AddComponent<AudioSource>();
+
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+
+        audioSource.Play();
+        Object.Destroy(audioObject, clip.length);
+
+        return audioSource;
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 [RequireComponent(typeof(BallMotor))]
 public class Player : MonoBehaviour
 {
+    [SerializeField] int _maxHealth = 3;
+    int _currentHealth;
+
     BallMotor _ballMotor;
 
     private void Awake()
     {
         _ballMotor = GetComponent<BallMotor>();
+    }
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
     }
 
     private void FixedUpdate()
@@ -26,5 +34,26 @@ public class Player : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical).normalized;
 
         _ballMotor.Move(movement);
+    }
+
+    public void IncreaseHealth(int amount)
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+        Debug.Log("Player's health: " + _currentHealth);
+    }
+
+    public void DecreaseHealth(int amount)
+    {
+        _currentHealth -= amount;
+        Debug.Log("Player's health: " + _currentHealth);
+        if (_currentHealth <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        gameObject.SetActive(false);
     }
 }
